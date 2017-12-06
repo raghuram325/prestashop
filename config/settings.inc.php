bdbce929e8ea818e3f0876e19166f004b578bc5c
raghuram325 <mraghuram325@gmail.com>
2017-12-06 12:52:46 +0530

    intial commmit

---
+++ config/settings.inc.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'prestashop');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', 'avish@2011');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'gCD0WNnoxYsHaSr7ktKoTsWIL3taoq7LIZZhQRyiqHsQQsVdJpNSDQHM');
+define('_COOKIE_IV_', 'EEMyJNaA');
+define('_PS_CREATION_DATE_', '2017-12-06');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.10');
+define('_RIJNDAEL_KEY_', 'bYAoxCszGCGGDpZuAGsyaFudHEwWf8RA');
+define('_RIJNDAEL_IV_', 'qge/K9ExmF2KoyJ5zxpQxQ==');
